9f4c83840ad04fa40f2d4cbb4ce9d699f424406b
flashfm <alex.flashfm@gmail.com>
2025-11-04 09:34:53 -0800

    WIP

--- makemore/04.py
+++ makemore/04.py
@@ -35,15 +35,19 @@ vocab_size = len(itos)
 n_embed = 10
 n_hidden = 200
 
-# Why those tensors have multiplies like 0, 0.01 or 0.2 - see at the end
+# Why those tensors have multiplies like 0, 0.01 or 0.2? See at the end
 
 C = torch.randn((vocab_size, n_embed), generator=g)
-W1 = torch.randn((n_embed * block_size, n_hidden), generator=g) * 0.2
+W1 = torch.randn((n_embed * block_size, n_hidden), generator=g) * 0.3 # Why 0.3 - see at the end
 b1 = torch.randn(n_hidden, generator=g) * 0.01
 W2 = torch.randn((n_hidden, vocab_size), generator=g) * 0.01
 b2 = torch.randn(vocab_size, generator=g) * 0
 
-parameters = [C, W1, b1, W2, b2]
+# See at the end why we added those
+bngain = torch.ones((1, n_hidden))
+bnbias = torch.zeros((1, n_hidden))
+
+parameters = [C, W1, b1, W2, b2, bngain, bnbias]
 print("Parameters:", sum(p.nelement() for p in parameters))
 for p in parameters:
     p.requires_grad = True
@@ -61,6 +65,13 @@ for i in range(max_steps):
     emb = C[Xb]
     embcat = emb.view(emb.shape[0], -1)
     hpreact = embcat @ W1 + b1
+
+    # apply batch normalization (details at the end of this program)
+    mean = hpreact.mean(0, keepdim=True)
+    std = hpreact.std(0, keepdim=True)
+    hpreact = (hpreact - mean) / std
+    hpreact = bngain * hpreact + bnbias
+
     h = torch.tanh(hpreact)
     logits = h @ W2 + b2
     loss = F.cross_entropy(logits, Yb)
@@ -103,7 +114,9 @@ def split_loss(split):
     }[split]
     emb = C[x]
     embcat = emb.view(emb.shape[0], -1)
-    h = torch.tanh(embcat @ W1 + b1)
+    hpreact = embcat @ W1 + b1
+    hpreact = bngain * (hpreact - hpreact.mean(0, keepdim=True)) / hpreact.std(0, keepdim=True) + bnbias # batch norm here as well
+    h = torch.tanh(hpreact)
     logits = h @ W2 + b2
     loss = F.cross_entropy(logits, y)
     print(split, loss.item())
@@ -128,6 +141,9 @@ def sample():
 
 # endregion
 
+# Let's fix initial weights, because they affect initial loss change.
+
+##### 1
 # Optimization 1: Fix softmax()
 
 # If we look at loss at the beginning, it is very high: 0/ 200000: 27.8817
@@ -140,6 +156,7 @@ def sample():
 # Simplest way to achieve that: multiply b2 and W2 by 0 (i.e. start them with all zeros).
 # However, it's better to start with W2 multiplied by a non-zero number like 0.01.
 
+##### 2
 # Optimization 2: Fix tanh() too saturated
 
 # 2nd problem is the tanh() function.
@@ -166,3 +183,46 @@ def sample():
 # (will be always in that flat region).
 
 # Solution is again, to multiply W2 and b2 by small numbers.
+
+#####
+# How do we select 0.01 or 0.1 for W1 and W2 multipliers?
+# If we check this example...
+#
+# x = torch.randn(1000, 10)
+# w = torch.randn(10, 200)
+# y = x @ w
+# print(x.mean(), x.std())
+# print(y.mean(), y.std())
+# plt.figure(figsize=(20,5))
+# plt.subplot(121)
+# plt.hist(x.view(-1).tolist(), 50, density=True)
+# plt.subplot(122)
+# plt.hist(y.view(-1).tolist(), 50, density=True)
+#
+# ... we'll see that while means of x and y are about 0, the standard deviation is changed from 1 in x to 3 in y - that's the result of multiplication.
+# The charts will also display a Gaussian and std() will be visible there.
+# Multiplying 'w' by a number >1 will make std() more, and <1 will make it less. Our goal is to make it 1 like in x.
+# How do we calculate exact number?
+# We'll refer to a paper "Delving deep into rectifiers: Surpassing Human-Level Performance on ImageNet Classification" by Kaiming He at al.
+# It defines that for ReLu, we need to use std = sqrt(2/input_size). 2 is called "gain" and derived from the fact that ReLU vanishes half of input.
+# Generic formula for std is "gain / sqrt(fan_mode)".
+# For tanh() gain is 5/3 (because it squashes the input) and "fan_mode" is "fan_in" = size of input.
+
+# If we check torch.randn(10000).std() it is 1, if it check (torch.randn(10000) * 0.2).std() - it is 0.2
+# So the multiplier is std!
+# For W1, (5/3)/((n_embed * block_size)**0.5) is about 0.3.
+
+
+##### 3
+# Optimization 3. Batch normalization.
+# Described in paper "Batch normalization: accelerating deep network training by reducing internal covariate shift" by Sergey Ioffe at al.
+# It says that we can normalize "hpreact" variable per every batch.
+# Normalize means that we want a Guassian distribution with 0 mean and 1 standard deviation.
+# For that, calculate mean and standard deviation of hpreact, and then do "hpreact = (hpreact - mean) / std".
+# Btw, mean and std are differentiable functions, so there will be no problem with backpropagation.
+
+# However, we want model to allow some deviation, i.e. allow the distribution to be shifted a bit, to avoid keeping it 0-mean, 1-std Guassian always.
+# So we introduce some "gain" and "shift" (bias) to be learned and modify the hpreact.
+# That's why we introduced bngain (all ones) and bnbias (all zeros) at the code above.
+
+# Batch normalization allows to think less about what "scale" multiplier should W1, W2, and so on have.
