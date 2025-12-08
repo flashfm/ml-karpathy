9b242726dfb1abae133115e5d681be05ad2d152b
flashfm <alex.flashfm@gmail.com>
2025-12-07 23:50:17 -0800

    WIP

--- gpt/02-gpt-2.py
+++ gpt/02-gpt-2.py
@@ -12,7 +12,7 @@
 from transformers import GPT2LMHeadModel, pipeline, set_seed
 from dataclasses import dataclass
 from torch.nn import functional as F
-import matplotlib.pyplot as plt, torch, torch.nn as nn, tiktoken, math, sys, os
+import matplotlib.pyplot as plt, torch, torch.nn as nn, tiktoken, math, sys, os, time
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from shared import get_file
@@ -71,6 +71,10 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
+        # we introduce this flag to scale init weights later in code
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -92,10 +96,16 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
 
-        att = (q @ k.transpose(-2,-1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T]==0, float("-inf"))
-        att = F.softmax(att, dim=-1)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        if device!="cuda":
+            att = (q @ k.transpose(-2,-1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:,:,:T,:T]==0, float("-inf"))
+            att = F.softmax(att, dim=-1)
+            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        else:
+            # The 4 lines above can be "fused" into a single fused kernel operation called FlashAttention (see below for details)
+            # But slow on Macbook, had to put under "if"
+            y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+
         y = y.transpose(1,2).contiguous().view(B, T, C) # re-assemble head outputs side by side
         y = self.c_proj(y)
         return y
@@ -111,6 +121,9 @@ class MLP(nn.Module):
         # It is not really needed now, but was used since the original version was slow in TensorFlow
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+        # we introduce this flag to scale init weights later in code
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -145,6 +158,43 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd)
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # In the original Attention is all you need paper they refer to another paper:
+        # "Using the Output Embedding to Improve Language Models" by Ofir Press and Lior Wolf.
+        # The idea is that semantically similar tokens (in the embedding matrix) should get similar probabilities in logits.
+        # So the weights of embedding and last linear layer (before softmax) are actually the same matrix.
+        self.transformer.wte.weight = self.lm_head.weight # Pointer copy!
+
+        self.apply(self._init_weights)
+
+    # Intitializes weights with random numbers
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            # normally std = 1/sqrt(number_of_features), number_of_features is embedding matrix size, for 127M model it's 768
+            # 1/sqrt(768) ~ 0.036 and 1/sqrt(1600) ~ 0.025
+            # but in GPT-2 they used constant 0.02
+
+            std = 0.02
+
+            # why do we multiply by 1/sqrt(N):
+            # due to residual stream (x = x + ...) in the Block class code, the standard deviation of values grows over layers
+            # example:
+            # x = torch.zeros(768)
+            # n = 100 # 100 layers
+            # for i in range(n):
+            #   x += torch.randn(768)
+            # print(x.std()) - gives ~ 10
+            # but we want ~ 1, so if we do instead x += n ** -0.5 * torch.randn(768)
+            # then x.std() will be ~ 1
+
+            if hasattr(module, "NANOGPT_SCALE_INIT"):
+                std *= (2 * self.config.n_layer) ** -0.5     # we do 2 * ... because  we have x = x + ... two times
+
+            torch.nn.init.normal(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal(module.weight, mean=0.0, std=0.02)
 
     # Forward is needed for generation from the model
     def forward(self, idx, targets=None):
@@ -231,8 +281,8 @@ print("Device:", device)
 num_return_sequences = 5
 max_length = 30
 enc = tiktoken.get_encoding("gpt2")
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
+torch.manual_seed(1337)
+torch.cuda.manual_seed(1337)
 
 def create_prompt(msg):
     tokens = enc.encode(msg) # 8 integers
@@ -279,25 +329,149 @@ def load_and_generate():
     print_sample(x)
 
 # load_and_generate()
+# prints samples that look like in try_huggingface(), though not exactly the same
 
 # Now, let's train an empty model on shakespeare dataset
 
 model = GPT(GPTConfig())
+model.to(device)
+
+# adds compilation time, but increases speed on GPU dramatically
+# compilation
+# a) removes Python interpreter from passes
+# b) reduces GPU read/write - decreases memory movements between GPU and GPU's mem (HBM) - also called "kernel fusion" - do more on GPU without moving to HBM
+model = torch.compile(model)
 
 text = get_file("shakespeare.txt", "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt")
-data = text[:1000]
-tokens = enc.encode(data)
 
 # From set of tokens, create batched inputs X and labels Y
 
-def create_training_batch():
+def create_training_batch(tokens):
     B,T = 4,32
     buf = torch.tensor(tokens[:B*T + 1])  # +1 so we can create Y as labels
+    buf = buf.to(device)
     # View as 2D creates batch rows
     x = buf[:-1].view(B,T)
     y = buf[1:].view(B,T)
     return x,y
 
-x,y = create_training_batch()
-logits, loss = model(x, y)
-print(loss)
+# x,y = create_training_batch(enc.encode(text[:1000]))
+# logits, loss = model(x, y)
+# print(loss)
+# since weights are random, should predict any token, so should be roughly ln(1/vocab_size) ~ 11
+# prints about 10.8
+
+# Let's optimize
+
+def optimize_single_batch(x, y):
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    for i in range(50):
+        optimizer.zero_grad()
+        _, loss = model(x, y)
+        loss.backward()
+        optimizer.step()
+        print(f"step {i}, loss: {loss.item()}")
+
+# optimize_single_batch(x, y)
+# step 49, loss: 0.002874101046472788 - overfitting the single batch (because we have just 1 batch at this point)
+
+class DataLoader:
+
+    def __init__(self, text, B, T):
+        self.B = B
+        self.T = T
+
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+
+        l = len(self.tokens)
+        print(f"tokens: {l}")
+        print(f"1 epoch = {l // (B*T)} batches")
+
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B*T + 1]  # +1 so we can create Y as labels
+        x = buf[:-1].view(B,T)
+        y = buf[1:].view(B,T)
+
+        self.current_position += B*T
+
+        # if next batch out of bounds - reset
+        if self.current_position + (B*T + 1) > len(self.tokens):
+            self.current_position = 0
+        
+        return x,y
+
+train_loader = DataLoader(text, B=16, T=1024)
+
+# with the default float32 numbers in matrices GPU does multiplication with highest precision
+# we are ok to decrease precision to increase speed
+# if so, float32 will be cut to TensorFloat32 (TF32) data type internally in GPU, transparently to our code
+# TF32 may not be available on Nvidia GPU prior to A100
+# if available, we will have 8x speed up.
+# this is only for multiplications in GPU, for other places we use autocast() later below
+torch.set_float32_matmul_precision("high")
+
+def gpu_sync():
+    if (device == "cuda"):
+        torch.cuda.synchronize()
+    elif (device == "mps"):
+        torch.mps.synchronize()
+
+def optimize():
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    for i in range(50):
+        t0 = time.time()
+
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+
+        # use autocasting of logits to bfloat16 (bf16)
+        # important: not float16, otherwise we will need to use "gradient scaler", which makes code more complex
+        # weights are still float32, so overall it is called "mixed precision" - some parts are bfloat16, anothers float32
+        if device=="cuda":
+            with torch.autocast(device_type=device, dtype=torch.bfloat16):
+                _, loss = model(x, y)
+        else:
+                _, loss = model(x, y)
+
+        loss.backward()
+        optimizer.step()
+
+        # Python works with GPU with async manner. To calculate time precisely, we want to sync here
+        gpu_sync()
+        t1 = time.time()
+        dt = (t1 - t0) * 1000 # time difference in ms
+        tokens_per_second = (train_loader.B * train_loader.T) / (t1 - t0)
+
+        print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_second:.2f}")
+
+optimize()
+
+# no compilation, with B=16, T=1024, on Macbook: step 0, loss: 10.935505867004395, dt: 46111.53ms, tok/sec: 355.31
+
+# nvidia-smi tool can show how GPUs are loaded - which GPU in use, how much memory used
+# the goal is to saturate memory using batch size (a lot of free mem - increase B, not enough - decrease), use powers of 2 for batch size
+
+# Steps to improve:
+
+# 1. decrease matrix multiplication precision to "high" (TF32 instead of float32)
+# on Macbook ~ same stats, since TF32 is not available
+
+# 2. use bfloat16 for calculations
+# on Macbook: much slower (dt: 68411.66ms, tok/sec: 239.49) - not clear why, had to disable
+# on A100 we expect: dt: 300ms, tok/sec: 54700
+
+# 3. use PyTorch compilation
+# on Macbook ~7x speedup: step 0, loss: 10.902034759521484, dt: 6972.98ms, tok/sec: 2349.64
+
+# 4. use FlashAttention
+# papers:
+# - "Online normalizer calculation for softmax" by Maxim Milakov et al.
+# - "FlashAttention: Fast and Memory-Efficient Exact Attention with IO-Awareness" by Tri Dao et al.
+# - "FlashAttention-2: Faster Attention with Better Parallelism and Work Partitioning" by Tri Dao
+# FlashAttention is kernel fusion operation. It uses more FLOPs but due to reduced use of HBM provides ~7x speedup.
+# on Macbook: much slower: dt: 97925.31ms, tok/sec: 167.31 - not clear why, had to disable
